893f5f5d55c15f04fb1222f5d6f7b38d5d155f6b
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-07-03 20:30:38 +0500

    set the navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,27 +2,27 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white flex items-center justify-between p-4 fixed w-full z-10 px-10">
-      <div className="text-xl font-semibold w-full">My Portfolio</div>
+    <nav className="bg-[#0A1930] flex items-center justify-between p-4 fixed w-full z-10 px-10 text-[#63B8B2]">
+      <div className="text-xl font-semibold w-full ">My Portfolio</div>
 
       <div className="flex items-center justify-center w-full">
-        <ul className="flex justify-around text-lg font-semibold w-full sm:w-3/4">
+        <ul className="flex justify-around text-lg font-semibold w-full sm:w-3/4 ">
           <li>
-            0. <span>Home</span>
+            0. <span className="text-gray-200">Home</span>
           </li>
           <li>
-            1. <span>About</span>
+            1. <span className="text-gray-200">About</span>
           </li>
           <li>
-            2. <span>Projects</span>
+            2. <span className="text-gray-200">Projects</span>
           </li>
           <li>
-            3. <span>Contact</span>
+            3. <span className="text-gray-200">Contact</span>
           </li>
         </ul>
 
-        <button className="ml-4 px-5 py-2 border text-white rounded">
-            Resume
+        <button className="ml-4 px-5 py-2 border border-[#63B8B2] rounded cursor-pointer text-[#63B8B2] hover:bg-[#63B8B2] hover:text-white transition-colors duration-300">
+          Resume
         </button>
       </div>
     </nav>
