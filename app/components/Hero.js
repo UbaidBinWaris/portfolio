4961289f6e9e27a39e1959b91b99da97e9fd9210
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-07-07 17:55:09 +0500

    make responsive

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -18,19 +18,21 @@ export const Hero = () => {
       <div className="absolute inset-0 bg-opacity-50 z-10" />
 
       <div className="flex flex-col md:flex-row justify-between px-10 items-center w-full h-full z-20">
-        <div className="flex h-1/2 flex-col justify-center items-center md:items-start text-center md:text-left md:max-w-1/2">
-          <h1 className="text-5xl md:text-6xl font-bold text-gray-600 text-center px-2">
+        <div className="flex h-1/2 flex-col justify-center items-center mt-20 md:items-start text-center md:text-left md:max-w-1/2">
+          <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold  text-gray-600 text-center whitespace-nowrap">
             &lt;h1&gt;
-            <span className="text-5xl md:text-6xl font-bold text-gray-100 letter-spacing- text-center px-4">
+            <span className="text-[clamp(1.5rem, 5vw, 4rem)] font-bold text-gray-100 px-2">
               Ubaid Bin Waris
             </span>
             &lt;/h1&gt;
           </h1>
 
-            <p className="text-lg md:text-xl text-start px-4 mt-6">
-              I am a Full Stack Developer with a passion for creating dynamic and responsive web applications. I specialize in both front-end and back-end development, ensuring seamless user experiences.
-            </p>
-          </div>
+          <p className="text-lg md:text-xl px-4 mt-6 text-justify">
+            I am a Full Stack Developer with a passion for creating dynamic and
+            responsive web applications. I specialize in both front-end and
+            back-end development, ensuring seamless user experiences.
+          </p>
+        </div>
         <div className="flex h-1/2 border-white">
           <Image
             src="/me.jpg"
