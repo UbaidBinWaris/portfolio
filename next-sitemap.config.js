1741600e4be98519e82d82e9184601d7b3bfa515
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-09-04 03:17:50 -0700

    upload and seo for google search

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://uabidbinwaris.dev", // your domain
+  generateRobotsTxt: true, // (optional) generates robots.txt
+  sitemapSize: 7000,
+};
