35c1d534032e361b468357583820ea6733bbf6a7
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-12-01 23:00:57 +0500

    feat: Simplify sitemap structure by removing unused routes and adjusting change frequency

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,38 +1,12 @@
 export default function sitemap() {
   const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL || 'https://uabidbinwaris.dev';
   
-  const routes = [
+  return [
     {
       url: siteUrl,
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: 'weekly',
       priority: 1.0,
     },
-    {
-      url: `${siteUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/#experience`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/#services`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.7,
-    },
-    {
-      url: `${siteUrl}/#contact`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.6,
-    },
   ];
-
-  return routes;
 }
